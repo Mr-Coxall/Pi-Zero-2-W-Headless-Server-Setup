0b0bb7dd42013005424554422df0ae87ea3640cf
Patrick Coxall <mr.coxall@mths.ca>
2021-12-22 16:47:47 -0500

    update

--- setup.sh
+++ setup.sh
@@ -18,11 +18,10 @@ cp ./.bashrc ~/.bashrc
 
 # load some programs
 echo Load programs
-sudo apt install tree
-sudo apt install unzip
 sudo apt-get -y install python3-pip
 
-# load .vimrc file
+# load vim & .vimrc file
+sudo apt install -y vim
 echo Load .vimrc file
 cp ./.vimrc ~/.vimrc
 
@@ -32,22 +31,22 @@ cp ./.gitconfig ~/.gitconfig
 
 # copy over shell script file
 echo Load shell script files
-mkdir ~/scripts
-cp ./repo.sh ~/scripts/repo.sh
-chmod +x ~/scripts/repo.sh
-cp ./git-push.sh ~/scripts/git-push.sh
-chmod +x ~/scripts/git-push.sh
-cp ./java-lint.sh ~/scripts/java-lint.sh
-chmod +x ~/scripts/java-lint.sh
-cp ./main.yml ~/scripts/main.yml
-cp ./swift.yml ~/scripts/swift.yml
+# mkdir ~/scripts
+# cp ./repo.sh ~/scripts/repo.sh
+# chmod +x ~/scripts/repo.sh
+# cp ./git-push.sh ~/scripts/git-push.sh
+# chmod +x ~/scripts/git-push.sh
+# cp ./java-lint.sh ~/scripts/java-lint.sh
+# chmod +x ~/scripts/java-lint.sh
+# cp ./main.yml ~/scripts/main.yml
+# cp ./swift.yml ~/scripts/swift.yml
 
 # load YouCompleteMe
 echo Load YouCompleteMe plugin for Vim
 # need to do an upgrade to python libraries first
-sudo pip3 install --upgrade requests
-sudo apt install vim-youcompleteme -y
-vim-addon-manager install youcompleteme
+# sudo pip3 install --upgrade requests
+# sudo apt install vim-youcompleteme -y
+# vim-addon-manager install youcompleteme
 
 # load java programming software
 echo load Java
@@ -61,49 +60,18 @@ cp ./checkstyle-8.44-all.jar ~/scripts/checkstyle.jar
 wget https://raw.githubusercontent.com/Mr-Coxall/dot_files/main/mr-coxall_checks.xml
 cp ./mr-coxall_checks.xml ~/scripts/
 
-
 # you might need to get a newer version of swift
-# https://swift.org/download/
-echo load Swift
-sudo apt-get install -y clang libblocksruntime0 libcurl4-openssl-dev -y
-wget https://swift.org/builds/swift-5.5.1-release/ubuntu2004/swift-5.5.1-RELEASE/swift-5.5.1-RELEASE-ubuntu20.04.tar.gz
-tar -zxvf swift-5.5.1-RELEASE-ubuntu20.04.tar.gz
-sudo mkdir /usr/bin/swift
-sudo cp -R ./swift-5.5.1-RELEASE-ubuntu20.04/usr/* /usr/bin/swift
-echo "" >> ~/.bashrc
-echo 'export PATH="${PATH}":/usr/bin/swift/bin' >> ~/.bashrc
 
 # SwiftLint
-# https://github.com/realm/SwiftLint/releases to get latest release
-echo load SwiftLint for Swift
-wget https://github.com/realm/SwiftLint/releases/download/0.45.0/swiftlint_linux.zip
-unzip -n swiftlint_linux.zip
-sudo mkdir /usr/bin/swiftlint
-sudo cp ./swiftlint /usr/bin/swiftlint/
-echo 'export PATH="${PATH}":/usr/bin/swiftlint' >> ~/.bashrc
-echo "" >> ~/.bashrc
-
-# Swift syntax highlighting for Vim
-# Original Source: http://wingsquare.com/blog/swift-script-syntax-highlighting-and-indentation-for-vim-text-editor/
-# Another helpful article: https://billyto.github.io/blog/swift-syntax-vim
-# More about Vim packages: http://vimcasts.org/episodes/packages/
-echo "--- creating ~/.vim/pack/bundle/start dir.."
-mkdir -p ~/.vim/pack/bundle/start
-echo "--- Cloning Apple's Swift repo.."
-git clone --depth=1 https://github.com/apple/swift/
-echo "--- Copying plugin to Vim bundles.."
-cp -r ./swift/utils/vim ~/.vim/pack/bundle/start/swift
-# echo "--- Cleaning up, removing swift repo.."
-# rm -rf ./swift/
 
 # install python linter
 pip3 install black
 
 # install C++
-sudo apt install -y g++
+# sudo apt install -y g++ - already installed on Pi
 pip3 install cpplint
-echo '# for CPPLint' >> ~/.bashrc
-echo 'export PATH="${PATH}":/home/ubuntu/.local/bin' >> ~/.bashrc
+# echo '# for CPPLint' >> ~/.bashrc
+# echo 'export PATH="${PATH}":/home/ubuntu/.local/bin' >> ~/.bashrc
 
 # Node.js
 # you might need to upgrade tp newer version: https://github.com/nvm-sh/nvm
@@ -120,11 +88,18 @@ npm install prompt-sync # note: not global flag - does not work with it!
 # npm install -g eslint
 # npm install -g eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin
 
+# load C#
+echo load C#
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel Current
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> ~/.bashrc
+echo 'export PATH=$PATH:$HOME/.dotnet' >> ~/.bashrc
+source ~/.bashrc
+
 # load GitHub CLI
 echo load GitHub CLI
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-sudo apt-add-repository https://cli.github.com/packages
-sudo apt install -y gh
+GITHUB_CLI_VERSION=$(curl -s "https://api.github.com/repos/cli/cli/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo gh.deb "https://github.com/cli/cli/releases/latest/download/gh_${GITHUB_CLI_VERSION}_linux_armv6.deb"
+sudo dpkg -i gh.deb
 
 # provision GitHub
 ssh-keygen -t ed25519 -C "mr.coxall@mths.ca"

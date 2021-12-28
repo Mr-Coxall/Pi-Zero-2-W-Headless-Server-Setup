7e3b0c7ec7c01bfd9b58fa2ac0d09a609ec51347
Patrick Coxall <mr.coxall@mths.ca>
2021-12-28 11:45:49 -0500

    swift update

--- setup.sh
+++ setup.sh
@@ -66,8 +66,38 @@ wget https://raw.githubusercontent.com/Mr-Coxall/dot_files/main/mr-coxall_checks
 cp ./mr-coxall_checks.xml ~/scripts/
 
 # you might need to get a newer version of swift
+# https://swift.org/download/
+echo load Swift
+sudo apt-get install -y clang libblocksruntime0 libcurl4-openssl-dev -y
+wget https://swift.org/builds/swift-5.5-release/ubuntu2004/swift-5.5-RELEASE/swift-5.5-RELEASE-ubuntu20.04.tar.gz
+tar -zxvf swift-5.5-RELEASE-ubuntu20.04.tar.gz
+sudo mkdir /usr/bin/swift
+sudo cp -R ./swift-5.5-RELEASE-ubuntu20.04/usr/* /usr/bin/swift
+echo "" >> ~/.bashrc
+echo 'export PATH="${PATH}":/usr/bin/swift/bin' >> ~/.bashrc
 
 # SwiftLint
+# https://github.com/realm/SwiftLint/releases to get latest release
+echo load SwiftLint for Swift
+wget https://github.com/realm/SwiftLint/releases/download/0.44.0/swiftlint_linux.zip
+unzip -n swiftlint_linux.zip
+sudo mkdir /usr/bin/swiftlint
+sudo cp ./swiftlint /usr/bin/swiftlint/
+echo 'export PATH="${PATH}":/usr/bin/swiftlint' >> ~/.bashrc
+echo "" >> ~/.bashrc
+
+# Swift syntax highlighting for Vim
+# Original Source: http://wingsquare.com/blog/swift-script-syntax-highlighting-and-indentation-for-vim-text-editor/
+# Another helpful article: https://billyto.github.io/blog/swift-syntax-vim
+# More about Vim packages: http://vimcasts.org/episodes/packages/
+echo "--- creating ~/.vim/pack/bundle/start dir.."
+mkdir -p ~/.vim/pack/bundle/start
+echo "--- Cloning Apple's Swift repo.."
+git clone --depth=1 https://github.com/apple/swift/
+echo "--- Copying plugin to Vim bundles.."
+cp -r ./swift/utils/vim ~/.vim/pack/bundle/start/swift
+# echo "--- Cleaning up, removing swift repo.."
+# rm -rf ./swift/
 
 # install python linter
 pip3 install black
